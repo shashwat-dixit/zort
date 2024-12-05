e57d74bfc6923a69854a4d4a7a044f7a7d5a865e
Shashwat Dixit <shashwatmain@gmail.com>
2024-12-06 01:08:19 +0530

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import { Poppins } from "next/font/google"
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         className="antialiased"
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
